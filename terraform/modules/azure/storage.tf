9306af8aaa868b5f76d56b6dcc3d755f31f88a24
John Ake <johnake@hotmail.com>
2024-05-21 15:35:28 +0100

    Add private link to storage account

--- terraform/modules/azure/storage.tf
+++ terraform/modules/azure/storage.tf
@@ -32,10 +32,10 @@ resource "azurerm_storage_account" "tempdata" {
 resource "azurerm_storage_container" "tempdata" {
   count                 = var.deploy_temp_data_storage_account ? 1 : 0
   name                  = "tempdata"
-  storage_account_name  = resource.azurerm_storage_account.tempdata[0].name
+  storage_account_name  = azurerm_storage_account.tempdata[0].name
   container_access_type = "private"
 }
-# to be created by TSC 
+# to be created by TSC
 resource "azurerm_subnet" "private_endpoint_subnet" {
   name                 = "privateEndpointSubnet"
   resource_group_name  = var.backend_resource_group_name
@@ -51,8 +51,28 @@ resource "azurerm_private_endpoint" "storage_private_endpoint" {
 
   private_service_connection {
     name                           = "tempdata-storage-private-service-connection"
-    private_connection_resource_id = resource.azurerm_storage_account.tempdata[0].id
+    private_connection_resource_id = azurerm_storage_account.tempdata[0].id
     is_manual_connection           = false
     subresource_names              = ["blob"]
   }
+}
+
+resource "azurerm_private_dns_zone" "privatelink_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.backend_resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_blob_vnet" {
+  name                  = "privatelink-blob-vnet-link"
+  resource_group_name   = var.backend_resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink_blob.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+}
+
+resource "azurerm_private_dns_a_record" "tempdata" {
+  name                = azurerm_storage_account.tempdata[0].primary_blob_endpoint
+  zone_name           = azurerm_private_dns_zone.privatelink_blob.name
+  resource_group_name = var.backend_resource_group_name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.storage_private_endpoint.private_service_connection[0].private_ip_address]
 }
